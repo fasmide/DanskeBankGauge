9ada30ffe9dd69156dc2ea08a6eb68ba1738e570
Kristian Mide <fas@mide.dk>
2019-02-07 21:02:33 +0100

    More firmware

--- firmware/firmware.ino
+++ firmware/firmware.ino
@@ -1,35 +1,68 @@
-#include <Adafruit_NeoPixel.h>
+#include "FastLED.h"
 
 
-#define PIN D5
-// How many NeoPixels are attached to the Arduino?
+#define PIXELPIN D5
 #define NUMPIXELS 7
- 
-Adafruit_NeoPixel pixels = Adafruit_NeoPixel(7, PIN, NEO_GRB + NEO_KHZ800);
 
-const int analogOutPin = 12; // D6
+// well the gauge is not 100% linear so at 10000 it reads more like 9950 and at 
+// 5000 it reads more like 5050 but its good enough for the girls i go out with...
+#define FULLSCALE 10400
+#define GAUGEPIN 12 // D6
+ 
+CRGB leds[NUMPIXELS];
 
 int outputValue = 0;
-uint32_t color = 0;
 
 void setup() {
+
+    // tell FastLED about the LED strip configuration
+    FastLED.addLeds<WS2811,PIXELPIN,GRB>(leds, NUMPIXELS).setCorrection(TypicalLEDStrip);
+    FastLED.setBrightness(255);
+
     // initialize serial communications at 9600 bps:
     Serial.begin(9600);
-    pixels.begin();
 }
 
 void loop() {
-    outputValue = random(0, 1024);
-    analogWrite(analogOutPin, outputValue);
+    if (Serial.available()) {
 
-    Serial.print("output = ");
-    Serial.println(outputValue);
-
-    color = pixels.Color(random(0, 255), random(0, 255),random(0, 255));
-    for(int i=0;i<NUMPIXELS;i++)
-    {
-        pixels.setPixelColor(i, color);
+        outputValue = Serial.parseInt();
     }
-    pixels.show();
-    delay(500);
+    analogWrite(GAUGEPIN, scale(outputValue));    
+    
+    EVERY_N_MILLISECONDS( 16 ) { animate(); FastLED.show(); }  
+
+}
+int counter = 0;
+void animate() {
+    // a colored dot sweeping back and forth, with fading trails
+    fadeToBlackBy( leds, NUMPIXELS, 10);
+    counter ++;
+    if (counter > NUMPIXELS) {
+        counter = 0;
+    } 
+    leds[counter] += CHSV( 0, 255, 255);
+
+}
+
+// we want to drift between 120 degrees and 0 on the hue weel (green to red)
+int scaleColor(int input) {
+    if (input <= -1000) {
+        return 0;
+    }
+    if (input >= 1000) {
+        return 120;
+    }
+    return map(input, -1000, 1000, 0, 120);
+}
+int scale(int input) {
+    if (input <= 0) {
+        return 0;
+    }
+    if (input >= FULLSCALE) {
+        return FULLSCALE;
+    }
+
+    // map(value, fromLow, fromHigh, toLow, toHigh)
+    return map(input, 0, FULLSCALE, 0, 1024);
 }
